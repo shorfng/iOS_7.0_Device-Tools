2b83062e0dd56ebb255f6cec3faed25b73e3eee3
shorfng <673974833@qq.com>
2016-09-22 19:10:07 +0800

    添加了 OC 版 单次定位的方法（之前没有写上）

--- 【定位服务】01 单次定位/OC - 单次定位/OC - 单次定位/ViewController.m
+++ 【定位服务】01 单次定位/OC - 单次定位/OC - 单次定位/ViewController.m
@@ -72,10 +72,15 @@
         // 开始更新位置信息（一旦调用了这个方法, 就会不断的刷新用户位置,然后告诉外界）
         // 以下代码默认只能在前台获取用户的位置信息,如果想要在后台获取用户的位置信息, 那么需要勾选后台模式 location updates
         // 小经验: 如果以后使用位置管理者这个对象, 实现某个服务,可以用startXX开始某个服务，stopXX停止某个服务
-        [self.locationM startUpdatingLocation];
+        // [self.locationM startUpdatingLocation];
         
         // 方法2：监听重大位置变化的服务(基于基站进行定位)（显著位置变化定位服务）
         //  [self.locationM startMonitoringSignificantLocationChanges];
+        
+        // 单次定位请求
+        // 必须实现代理的定位失败方法
+        // 不能与startUpdatingLocation方法同时使用
+        [self.locationM requestLocation];
     } else {
         NSLog(@"没有开启定位服务");
     }
@@ -106,6 +111,9 @@
     [manager stopUpdatingLocation];
 }
 
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"定位失败");
+}
 
 #pragma mark - 代理方法：当用户授权状态发生变化时调用
 -(void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
