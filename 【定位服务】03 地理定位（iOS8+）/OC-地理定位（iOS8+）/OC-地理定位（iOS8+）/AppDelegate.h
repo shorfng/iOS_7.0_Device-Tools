601cd9e60ccc9b042dd10fda82247abe41938d6e
shorfng <673974833@qq.com>
2016-09-23 12:39:51 +0800

    OC-地理定位（iOS8+）

---
+++ 【定位服务】03 地理定位（iOS8+）/OC-地理定位（iOS8+）/OC-地理定位（iOS8+）/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC-地理定位（iOS8+）
+//
+//  Created by 蓝田 on 2016/9/22.
+//  Copyright © 2016年 Loto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 【定位服务】03 地理定位（iOS8+）/OC-地理定位（iOS8+）/OC-地理定位（iOS8+）/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC-地理定位（iOS8+）
+//
+//  Created by 蓝田 on 2016/9/22.
+//  Copyright © 2016年 Loto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
