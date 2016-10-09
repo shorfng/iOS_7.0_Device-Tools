c5ef58109e3db6a61605577bba9dc3248b576c63
shorfng <673974833@qq.com>
2016-10-09 11:41:48 +0800

    OC - 大头针场景模拟

---
+++ 【定位服务】14 大头针场景模拟/OC - 大头针场景模拟/OC - 大头针场景模拟/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC - 大头针场景模拟
+//
+//  Created by 蓝田 on 2016/10/8.
+//  Copyright © 2016年 Loto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 【定位服务】14 大头针场景模拟/OC - 大头针场景模拟/OC - 大头针场景模拟/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OC - 大头针场景模拟
+//
+//  Created by 蓝田 on 2016/10/8.
+//  Copyright © 2016年 Loto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
